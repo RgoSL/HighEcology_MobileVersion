5f04d84bb48cdcffab7806edc958bd6569eaa7d9
Diggo 🤙🗿🤙 <125403873+RgoSL@users.noreply.github.com>
2024-11-25 11:34:29 -0300

    Delete HighEcology directory

--- HighEcology/app/src/main/java/com/example/highecology/model/Curso.kt
+++
@@ -1,9 +0,0 @@
-package com.example.highecology.model
-//File Kotlin que Vai Ser Usado Como Classe de Valores dos Elementos que Serão Passados Para o Card
-
-data class Curso(
-    val imgCurso: Int? = null,
-    val nomeCurso: String? = null,
-    val desCurso: String? = null,
-    val dur: String? = null
-)
--- HighEcology/app/src/main/java/com/example/highecology/viewModel/CursoViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.highecology.viewModel
-
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewmodel.viewModelFactory
-
